5f06f2a84b3b674f957af4b3d64b96dcc70eba6c
Sri Prasanna <k.sriprasanna@gmail.com>
2014-11-26 15:02:54 -0500

    [#602] - arreglando url

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ MockRespuesta::Application.routes.draw do
   get 'paises/:id' => 'api#obtener_pais_con_id'
   get 'archivos' => 'api#obtener_archivo_con_ids'
   post 'firmadigital' => 'api#firma_digital'
-  post 'firmadigital/credentiales/verificar' => 'api#verificar_contrasenia'
+  post 'firmadigital/credenciales/verificar' => 'api#verificar_contrasenia'
 end
