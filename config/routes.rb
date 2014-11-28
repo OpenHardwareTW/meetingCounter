5b8183af099337f502c44e3cce065d1ef236d497
Sri Prasanna <k.sriprasanna@gmail.com>
2014-11-28 14:30:12 -0500

    [#602] - cambios ruta de firmar

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 MockRespuesta::Application.routes.draw do
   get 'paises/:id' => 'api#obtener_pais_con_id'
   get 'archivos' => 'api#obtener_archivo_con_ids'
-  post 'firmadigital' => 'api#firma_digital'
-  post 'firmadigital/credenciales/verificar' => 'api#verificar_contrasenia'
+  post 'firmaDigital' => 'api#firma_digital'
+  post 'firmaDigital/credenciales/verificar' => 'api#verificar_contrasenia'
 end
