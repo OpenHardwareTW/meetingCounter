66d0e5e302c0e71fef09e0d1d72cabcdab449b17
Sri Prasanna <k.sriprasanna@gmail.com>
2014-11-28 14:49:11 -0500

    [#602] - cambios ruta para verificar contrasenia

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ MockRespuesta::Application.routes.draw do
   get 'paises/:id' => 'api#obtener_pais_con_id'
   get 'archivos' => 'api#obtener_archivo_con_ids'
   post 'firmaDigital' => 'api#firma_digital'
-  post 'firmaDigital/credenciales/verificar' => 'api#verificar_contrasenia'
+  post 'firmaDigital/credenciales/validar' => 'api#verificar_contrasenia'
 end
