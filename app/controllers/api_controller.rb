201ada74267b87db32612ed6e4b75ae6d9ad36b7
Roberto Paredes <rdparedessalazar@gmail.com>
2015-07-09 19:18:47 -0500

    contador y distancia

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -1,12 +1,12 @@
 require 'json'
 
 class ApiController < ApplicationController
-  before_filter :validar_authtoken, except: [:obtener_pais_con_id, :consultar_discapacidad, :subir_archivo, :return_nombre_para_regimen, :regimenes, :contarPersonas]
   protect_from_forgery only: [:nothing]
 
   $contador = 0
 
   def contarPersonas
+
     if params[:estado].to_i == 1
       $contador += 1
     else
@@ -16,156 +16,18 @@ class ApiController < ApplicationController
       end
     end
 
-    print $contador
-    respuesta = { 'contador': $contador }
+    distancia = params[:distancia]
+
+    respuesta = { 'contador': $contador, 'distancia': distancia }
     render_response :created, respuesta
   end
 
+  def reiniciarContador
+    $contador = 0
 
-
-  # retornar 404 cuando id es no Integer
-  # retornar mock cuando id es Integer
-  def obtener_pais_con_id
-    if params[:id].to_i == 0
-      return render_response :not_found
-    end
-    response = { id: params[:id], nombre: random_word }
-    render_response :ok, response
-  end
-
-  # retornar 404 cuando id es no Integer
-  # retornar mock cuando id es Integer
-  def obtener_pais_con_secuencial
-    if params[:secuencial].to_i == 0
-      return render_response :not_found
-    end
-    response = { secuencial: params[:secuencial], nombre: random_word }
-    render_response :ok, response
-  end
-
-  def obtener_archivo_con_ids
-    archivos = {
-              elementos: [
-                          {
-                              id: 130,
-                              nombre: "NihpMMjSfO4909040394484558758.doc",
-                              ruta: "/tmp/2404f5b8-d21d-4c30-92e2-2f5c2311c39e-NihpMMjSfO4909040394484558758.doc"
-                          },
-                          {
-                              id: 131,
-                              nombre: "dYOvuxPDfY5838682484897547294.doc",
-                              ruta: "/tmp/6dcd2775-78fe-42a5-b291-cc098a2e7033-dYOvuxPDfY5838682484897547294.doc"
-                          }
-                      ]
-          }
-
-    unless params[:hash].blank?
-      archivos[:elementos] = archivos[:elementos].collect do |elemento|
-        elemento[:hash] = random_word
-        elemento[:algoritmoHash] = params[:hash]
-        elemento
-      end
-    end
-
-    render_response :ok, archivos
-  end
-
-  def firma_digital
-    parametros = [params[:nombreUsuario], params[:textoAFirmar], params[:contrasenia]].select{ |x| !x.blank? }
-    if parametros.count < 3
-      return render_response :unprocessable_entity
-    end
-    response = { id: random_word }
-    render_response :ok, response
-  end
-
-  def verificar_contrasenia
-    if params[:contrasenia].length < 10
-     return render_response :not_found
-    end
-    render_response :ok
-  end
-
-  def personas
-    names = []
-    10.times do
-      names << {nombre: random_word}
-    end
-    render_response :ok, names
-  end
-
-  def porToken
-    user = {permisosArbol: random_word, institucion: {idInstitucion: 1001}}
-    render_response :ok, user
-  end
-
-  def permisosPorToken
-    permisos = {elementos:[{id:1, acceso: 'CREAR', modulo: 'Administración de usuarios', funcion: 'Gestionar Usuarios', nombrePermiso: 'crear_usuarios'}]}
-    render_response :ok, permisos
-  end
-
-  def cantones
-    cantones = {elementos:[{id:'0101', nombre:'Cuenca', idZona:'06'}, {id:'0102', nombre:'Giron', idZona:'06'}, {id:'0103', nombre:'Gualaceo', idZona:'06'}]}
-    render_response :ok, cantones
-  end
-
-  def validar_token
-    usuario = request.headers['x-token'] == 'token' ? 'admin' : ''
-    render text: usuario
-  end
-
-  def consultar_discapacidad
-    discapacidad = {deficienciaPredomina:'fisica'}
-    render_response :ok, discapacidad
-  end
-
-  def subir_archivo
-    render text: rand(99999), status: :created
-  end
-
-  def return_nombre_para_regimen
-    custom_endpoint = ["cursosAcademicos", "titulaciones"]
-    if custom_endpoint.include?(params[:endpoint]) and params[:regimen] == "2013"
-      key = params[:endpoint] == custom_endpoint.first ? :cursoNombre : :titulacionNombre
-      response = {}
-      response[key] = {nombre: random_word}
-      render_response :ok, response
-    else
-      render_response :ok, {nombre: random_word}
-    end
-  end
-
-  def return_created
-    render_response :created
-  end
-
-  def guardar_titulaciones
-    render_response :created, {}
-  end
-  
-  def regimenes
-    json = {
-       elementos: [{
-          id: 1,
-          nombre: "2009"
-       }, {
-          id: 2,
-          nombre: "2013"
-       }]
-    }
-    render_response :ok, json
-  end
-
-  private
-  def random_word
-    (0...10).map { ('a'..'z').to_a[rand(26)] }.join
-  end
-
-  def validar_authtoken
-    unless ( request.headers['Authorization'] || request.headers['x-token'] )
-      return render_response :unauthorized
-    end
-  end
+    respuesta = { 'contador': $contador }
+    render_response :created, respuesta
+  end 
 
   def render_response status, json = {}
     render json: json, status: status
--- config/routes.rb
+++ config/routes.rb
@@ -1,23 +1,4 @@
 MockRespuesta::Application.routes.draw do
-  
-  get 'paises/:id' => 'api#obtener_pais_con_id'
-  get 'paises/secuencial/:secuencial' => 'api#obtener_pais_con_secuencial'
-  get 'ciudades/:id' => 'api#obtener_pais_con_id'
-  get 'archivos' => 'api#obtener_archivo_con_ids'
-  get 'personas' => 'api#personas'
-  get 'usuario/porToken' => 'api#porToken'
-  get 'permisos/porToken' => 'api#permisosPorToken'
-  post 'firmaDigital' => 'api#firma_digital'
-  post 'firmaDigital/credenciales/validar' => 'api#verificar_contrasenia'
-  get 'cantones' => 'api#cantones'
-  get 'token/validar' => 'api#validar_token'
-  get 'discapacidad/:identificacion' => 'api#consultar_discapacidad'
-  post 'archivos/subir' => 'api#subir_archivo'
-  get ":regimen/instituciones/nacionales/:id" => 'api#return_nombre_para_regimen'
-  get ":regimen/:endpoint/:id" => 'api#return_nombre_para_regimen'
-  post "archivos/:id/pdf" => 'api#return_created'
-  post "titulaciones/:archivoId" => "api#guardar_titulaciones"
-  get "2013/regimenes" => "api#regimenes"
-  
   post "contador/personas/:estado" => "api#contarPersonas" 
+  post "contador/reset" => "api#reiniciarContador"
 end
